34b5077bb12f9d79a247e515560d8cd5b4906cf1
Jim-othy <72723971+jim-othy@users.noreply.github.com>
2023-02-22 21:29:58 -0600

    Resource Loader
    added resources to a seperate JS file to be loaded in, soon adding alot of resources, makes it easier to go through them and fix them + may add them inside of an array inside of as variables

---
+++ public/Resources/Landing/index.js
@@ -0,0 +1,4 @@
+var heading = document.head.innerHTML
+var fontawesome = "<script src=\"https://kit.fontawesome.com/25d9666422.js\" crossorigin=\"anonymous\"></script>";
+var homepagecss = "<link rel=\"stylesheet\" href=\"https://j-imsite.web.app/Resources/Landing/home.css\">";
+document.head.innerHTML =  heading + fontawesome + homepagecss
