4232e573c03e826787b7bde2e8e72fc354ee6ac7
Jim-othy <72723971+jim-othy@users.noreply.github.com>
2023-03-01 18:35:22 -0600

    tweaks

--- public/Resources/Landing/index.js
+++ public/Resources/Landing/index.js
@@ -2,5 +2,6 @@ var heading = document.head.innerHTML
 var resources = [
     '<script src=\"https://kit.fontawesome.com/25d9666422.js\" crossorigin=\"anonymous\"></script>', // Fontawesome
     '<link rel=\"stylesheet\" href=\"https://j-imsite.web.app/Resources/Landing/home.css\">', // Index CSS
+    '<link rel=\"stylesheet\" href=\"./Resources/Landing/home.css\">'
 ]
 document.head.innerHTML =  heading + resources
