004fded761c38627ecb7e306fa069088bdd6119e
Jim-othy <72723971+jim-othy@users.noreply.github.com>
2023-04-05 16:38:37 -0500

    Rewrite

--- public/Resources/load.js
+++ public/Resources/load.js
@@ -1,12 +1,16 @@
-var speed = 'slow';
-$('html, body').hide();
-$(document).ready(function() {
-  $('html, body').fadeIn(speed, function() {
-    $('a[href], button[href]').click(function(event) {
-      var url = $(this).attr('href');
-      if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
+const animationSpeed = 'slow';
+const $htmlBody = $('html, body');
+const isLocalLink = (url) => url.startsWith('#') || url.startsWith('javascript:');
+
+$htmlBody.hide();
+
+$(document).ready(() => {
+  $htmlBody.fadeIn(animationSpeed, () => {
+    $('a[href], button[href]').on('click', (event) => {
+      const url = $(event.currentTarget).attr('href');
+      if (isLocalLink(url)) return;
       event.preventDefault();
-      $('html, body').fadeOut(speed, function() {
+      $htmlBody.fadeOut(animationSpeed, () => {
         window.location = url;
       });
     });
