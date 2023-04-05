f14cf0f46a1c7ed7e9b74f345f7e4127584ab28d
Jim-othy <72723971+jim-othy@users.noreply.github.com>
2023-04-05 16:38:42 -0500

    Rewrite

--- public/Resources/Scripts/index.js
+++ public/Resources/Scripts/index.js
@@ -1,24 +1,28 @@
+const smoothScroll = (event) => {
+  const target = $(this.hash);
+  const $target = $(target);
+  const isTargetFocused = $target.is(":focus");
+  
+  if (target.length && !isTargetFocused) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 1000, () => {
+      $target.attr('tabindex', '-1').focus();
+    });
+  }
+};
+
 $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) {
-            return false;
-          } else {
-            $target.attr('tabindex','-1');
-            $target.focus();
-          }
-        });
-      }
+  .on('click', (event) => {
+    const pathname = location.pathname.replace(/^\//, '');
+    const currentPathname = this.pathname.replace(/^\//, '');
+    const hostname = location.hostname;
+    const currentHostname = this.hostname;
+
+    if (pathname === currentPathname && hostname === currentHostname) {
+      smoothScroll(event);
     }
   });
